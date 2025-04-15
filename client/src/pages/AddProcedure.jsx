17c88937c935d1d7394802ce233167d3c1984067
Chittapuli Arya Vardhan Padal <144430459+Aryavardhan21@users.noreply.github.com>
2025-04-15 10:49:39 +0530

    Create AddProcedure.jsx
    added the Addprocedure page in the client/pages

---
+++ client/src/pages/AddProcedure.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { AddProcedure } from "../components/AddProcedure";
+const Add_Procedure = () => {
+  return (
+    <>
+      <AddProcedure />
+    </>
+  );
+};
+
+export default Add_Procedure;
