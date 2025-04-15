e6899bac51dc521d2156328dd3b7f3735d8f1b5e
Chittapuli Arya Vardhan Padal <144430459+Aryavardhan21@users.noreply.github.com>
2025-04-15 12:02:47 +0530

    Create procedure.jsx

---
+++ client/src/pages/procedure.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ProcedureTable } from "../components/procedure";
+const Procedure = () => {
+  return (
+    <>
+      <ProcedureTable />
+    </>
+  );
+};
+
+export default Procedure;
