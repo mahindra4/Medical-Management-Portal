86c9c58b75e10e65b517cd540fe2ef2e96bbf970
Chittapuli Arya Vardhan Padal <144430459+Aryavardhan21@users.noreply.github.com>
2025-04-15 11:23:41 +0530

    Create UpdateProcedure.jsx

---
+++ client/src/pages/UpdateProcedure.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UpdateProcedure from '../components/UpdateProcedure';
+
+const UpdateProcedureComponent = () => {
+  return (
+    <>    
+      <UpdateProcedure />
+    </>
+  );
+}
+
+export default UpdateProcedureComponent;
