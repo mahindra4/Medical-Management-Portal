14a6c9bb75d2d51cabe24c926323e950caa702fe
Chittapuli Arya Vardhan Padal <144430459+Aryavardhan21@users.noreply.github.com>
2025-04-26 14:12:46 +0530

    Update PageLayout.jsx
    added hospital in the layout

--- client/src/layouts/PageLayout.jsx
+++ client/src/layouts/PageLayout.jsx
@@ -39,6 +39,7 @@ import {
   ChevronDownIcon,
   Bars3Icon,
   XMarkIcon,
+  BuildingOfficeIcon,
 } from "@heroicons/react/24/outline";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
@@ -589,6 +590,71 @@ const Layout = ({ children }) => {
                     )}
                   </Accordion>
                 )}
+{hasRequiredRole(roleArr, ["ADD_HOSPITAL", "HOSPITAL_LIST"]) && (
+                  <Accordion
+                    open={open === 11}
+                    icon={
+                      !(isCollapsed & !isHovered) && (
+                        <ChevronDownIcon
+                          style={{ color: "#f1ffea" }}
+                          strokeWidth={2.5}
+                          className={`mx-auto h-4 w-4 transition-transform ${
+                            open === 11 ? "rotate-180" : ""
+                          }`}
+                        />
+                      )
+                    }
+                  >
+                    <ListItem className="p-0" selected={open === 9}>
+                      <AccordionHeader
+                        onClick={() => handleOpen(11)}
+                        className="border-b-0 p-3"
+                      >
+                        <ListItemPrefix>
+                          <BuildingOfficeIcon
+                            className="h-5 w-5"
+                            style={{ color: "#f1ffea" }}
+                          />
+                        </ListItemPrefix>
+                        {!(isCollapsed & !isHovered) && (
+                          <Typography
+                            style={{ color: "#f1ffea" }}
+                            className="mr-auto font-normal"
+                          >
+                            Hospital
+                          </Typography>
+                        )}
+                      </AccordionHeader>
+                    </ListItem>
+                    {!(isCollapsed & !isHovered) && (
+                      <AccordionBody className="py-1">
+                        <List className="p-0" style={{ color: "#f1ffea" }}>
+                        {hasRequiredRole(roleArr, ["ADD_HOSPITAL"]) && <ListItem
+                            className="ml-9"
+                            onClick={() => navigate("/hospital/add")}
+                          >
+                            ADD HOSPITAL
+                          </ListItem>}
+                          {hasRequiredRole(roleArr, ["HOSPITAL_LIST"]) && <ListItem
+                            className="ml-9"
+                            onClick={() => navigate("/hospitals")}
+                          >
+                            HOSPITAL LIST
+                          </ListItem>}
+                        </List>
+                      </AccordionBody>
+                    )}
+                  </Accordion>
+                )}
+
+
+
+
+
+
+
+
+                
                 {hasRequiredRole(roleArr, ["ADD_PATIENT", "PATIENT_LIST"]) &&  (
                   <Accordion
                     open={open === 4}
