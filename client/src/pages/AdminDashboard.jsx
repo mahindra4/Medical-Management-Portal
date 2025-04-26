75e784ffd178c7bae750e6d3e51328e83589b84b
Madhu. Siripurapu <120372287+Gabbumaay@users.noreply.github.com>
2025-04-26 23:13:47 +0530

    Update AdminDashboard.jsx

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -22,6 +22,7 @@ import { useNavigate } from "react-router-dom";
 import { SyncLoadingScreen } from "../components/UI/LoadingScreen";
 import axios from "axios";
 import { apiRoutes } from "../utils/apiRoutes";
+import VisitingSpecialistBanner from "../components/VisitingSpecialistBanner";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -226,6 +227,8 @@ const AdminDashboard = () => {
       {loading && <SyncLoadingScreen />}
       {!loading && <Layout>
         <div>
+          {/* Visiting Specialist Banner */}
+          <VisitingSpecialistBanner />
           <div className="grid-container -mt-7">
             <div className="mt-6 ml-4 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl grid-item">
               <div className="p-6">
